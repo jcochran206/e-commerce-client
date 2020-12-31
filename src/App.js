0a7a8177689d33f286ad00a19877b965d4a0295e
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-12-30 23:02:51 -0800

    added react-bootstrap and font awesome cdn, modified header and footer, and little css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to our Proshop</h1>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to our Proshop</h1>
+        </Container>
+      </main>
+      <Footer/>
     </div>
   );
 }
