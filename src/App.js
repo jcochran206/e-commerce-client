0ddc2a4c11940bdd7cc890da3c253322ec32f98b
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2021-01-03 13:33:10 -0800

    added product screen and routers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <main className="py-3">
-        <Container>
-          <HomeScreen/>
-        </Container>
-      </main>
-      <Footer/>
-    </div>
+    <Router>
+      <Switch>
+        <div className="App">
+          <Header/>
+          <main className="py-3">
+            <Container>
+              <Route path='/' component={HomeScreen} exact />
+              <Route path='/product/:id' component={ProductScreen} />
+            </Container>
+          </main>
+          <Footer/>
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <div>
+            product screen
+        </div>
+    )
+}
+
+export default ProductScreen
